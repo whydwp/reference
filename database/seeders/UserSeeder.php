0e1a2172a3ba163cbc3f4a8b7f950da4c85c5f15
ahmadshobirin <ahmadshobirin.dev@gmail.com>
2021-03-31 00:44:22 +0700

    setup spatie role and permisssion

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -6,6 +6,8 @@ use App\Models\User;
 use App\Models\UserType;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
 
 class UserSeeder extends Seeder
 {
@@ -53,8 +55,55 @@ class UserSeeder extends Seeder
             ],
         ];
 
+        $permissions = [
+            'user-list',
+            'user-create',
+            'user-edit',
+            'user-delete',
+
+            'kategori-list',
+            'kategori-create',
+            'kategori-edit',
+            'kategori-delete',
+
+            'dokumen-list',
+            'dokumen-create',
+            'dokumen-edit',
+            'dokumen-delete',
+
+            'ebook-list',
+            'ebook-create',
+            'ebook-edit',
+            'ebook-delete',
+
+            'komentar-list',
+            'komentar-create',
+            'komentar-edit',
+            'komentar-delete',
+
+            'dashboard-admin',
+            'profile',
+            'reference',
+            'like'
+         ];
+
+        $roles = [
+            "superadmin",
+            "admin",
+            "pnlpusdiklat"
+        ];
+
+
+        foreach ($permissions as $permission) {
+            Permission::create(['name' => $permission]);
+        }
+
+        foreach ($roles as $role) {
+            Role::create(['name' => $role]);
+        }
+
         UserType::insert($userTypes);
-        $this->command->info("user type success cfreated");
+        $this->command->info("user type success created");
 
         User::insert($users);
         $this->command->info("user success cfreated");
