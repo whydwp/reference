a9b7bf34b1506f59d6cabee919178ea9d1db87bc
ahmadshobirin <ahmadshobirin.dev@gmail.com>
2021-03-31 00:30:26 +0700

    created migration and seeder from scrat

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,7 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
+use App\Models\UserType;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -13,13 +16,47 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        $user =  new \App\Models\User;
-        $user->username = 201710370311320;
-        $user->full_name = "wahyu";
-        $user->email = "administrator@mail.com";
-        $user->password = \Hash::make("admin");
-        $user->user_type_id = 1;
-        $user->save();
-        $this->command->info("user Admin Berhasil Dibuat");
+        $userTypes = [
+            [
+                "type"        => "superadmin",
+                "created_at"  => now(),
+                "modified_at" => now(),
+            ],
+            [
+                "type"        => "admin",
+                "created_at"  => now(),
+                "modified_at" => now(),
+            ],
+            [
+                "type"        => "pusdiklat",
+                "created_at"  => now(),
+                "modified_at" => now(),
+            ],
+        ];
+
+        $users = [
+            [
+                "username"     => "201710370311320",
+                "full_name"    => "wahyu",
+                "email"        => "administrator@mail.com",
+                "password"     => Hash::make("admin"),
+                "user_type_id" => 1,
+                "created_at"   => now(),
+            ],
+            [
+                "username"     => "srob",
+                "full_name"    => "srob",
+                "email"        => "srob@mail.com",
+                "password"     => Hash::make("srob"),
+                "user_type_id" => 1,
+                "created_at"   => now(),
+            ],
+        ];
+
+        UserType::insert($userTypes);
+        $this->command->info("user type success cfreated");
+
+        User::insert($users);
+        $this->command->info("user success cfreated");
     }
 }
