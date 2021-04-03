4a2edb381c9ce0db216c2ca280c6a8c313d694e5
whydwp <wahyu.purwanto258@gmail.com>
2021-04-04 05:25:49 +0700

    reference

--- resources/views/reference/index.blade.php
+++ resources/views/reference/index.blade.php
@@ -147,6 +147,9 @@ Reference
                                     </div>
                                 </div>
                                 <div class="row">
+                                    <div class="col-md-3">
+                                        
+                                    </div>
                                     <div class="col-md-3" style="margin-right: -5px !important;">
                                         <form method="get" action="{{route('reference.index')}}">
                                             <div class="form-gruop">
