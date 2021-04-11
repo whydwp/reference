f5612ac47f38662cbcd210bef203bc14bbf4f153
ahmadshobirin <ahmadshobirin.dev@gmail.com>
2021-04-11 23:42:45 +0700

    user edit

--- app/Http/Controllers/ProfilController.php
+++ app/Http/Controllers/ProfilController.php
@@ -68,26 +68,16 @@ class ProfilController extends Controller
 
     public function update(Request $request, $id)
     {
-        $user = User::find($id);
-
-        $input = $request->all();
-
         $rules = [
             'full_name' => 'required|max:250',
-            // 'username' => 'required|max:100|unique:users,username,' . $id,
-            'email' => 'required|email|max:255|unique:users,email,' . $id,
-            'avatar_file' =>  'sometimes|nullable|mimes:jpeg,jpg,png,pdf|max:5048',
-
-
-
-            ];
+            'email' => 'required|email|max:255|unique:user_auth,email,' . $id,
+            'avatar_file' =>  'sometimes|nullable|mimes:jpeg,jpg,png|max:5048',
+        ];
 
         $messages = [
             'full_name.required'          => 'Nama wajib diisi.',
-            // 'username.required'      => 'username wajib diisi.',
             'email.required'           => 'email wajib diisi.',
             'avatar_file.required'         => 'fotomu wajib diisi.',
-
         ];
 
         $validator = Validator::make($request->all(), $rules, $messages);
@@ -95,35 +85,13 @@ class ProfilController extends Controller
         if ($validator->fails()) {
             return redirect()->route('profil.index')->withErrors($validator)->withInput($request->all());
         }
-        // $validasi = Validator::make($input, [
-        //     'full_name' => 'required|max:250',
-        //     'username' => 'required|max:100|unique:users,username,' . $id,
-        //     'email' => 'required|email|max:255|unique:users,email,' . $id,
-        //     'password' => 'sometimes|nullable|min:6',
-        //     'avatar_file' =>  'sometimes|nullable|mimes:jpeg,jpg,png,pdf|max:5048',
 
-
-        // ]);
-
-        // if ($validasi->fails()) {
-        //     return redirect()->route('profil.index')->withErrors($validasi);
-        // }
-        // dd($user);
-
-        // $user->update([
-        //     'full_name' => $request->full_name,
-        //     'username' => $request->username,
-        //     'email' => $request->email,
-        //     'password' => Hash::make($request->password),
-        //     'avatar_file' => $request->avatar_file
-
-        // ]);
-            $user->full_name = $request->full_name;
-            // $user->username = $request->username;
-            $user->email = $request->email;
-            if($request->password != NULL) {
-                $user->password = Hash::make($request->password);
-            }
+        $user = User::find($id);
+        $user->full_name = $request->full_name;
+        $user->email     = $request->email;
+        if($request->password != NULL) {
+            $user->password = Hash::make($request->password);
+        }
 
         if ($request->hasFile('avatar_file')) {
             if ($request->file('avatar_file')->isValid()) {
