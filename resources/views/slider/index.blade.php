9107f02318caf3a81bf92a6499fb5a4614d5a4c8
whydwp <wahyu.purwanto258@gmail.com>
2021-04-30 02:35:08 +0700

    update 27 04 21

--- resources/views/slider/index.blade.php
+++ resources/views/slider/index.blade.php
@@ -59,7 +59,7 @@ Document
                         <div class="modal-dialog">
                             <div class="modal-content">
                                 <div class="modal-header">
-                                    <h4 class="modal-title">Kategori</h4>
+                                    <h4 class="modal-title">Slide</h4>
                                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                                         <span aria-hidden="true">&times;</span>
                                     </button>
